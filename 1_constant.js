fbbc87a4335c261413f00bc122b1d64fbd3555e5
pandurang sadgir <psadgir111@gmail.com>
2023-03-07 23:45:48 +0530

    constant in javascript

---
+++ 1_constant.js
@@ -0,0 +1,21 @@
+// Constant is just declared
+const userName;
+console.log(userName);
+// output
+// SyntaxError: Missing initializer in const declaration
+
+
+// constant declare and initialize immediatly
+const clientName = 'jonny depp';
+console.log(clientName);
+// output
+// $ node 1_constant.js
+// jonny depp
+
+
+// Trying to change the value of constant
+const message = 'Hello js';
+message = 'bye bye old trends';
+console.log(message);
+// output
+// TypeError: Assignment to constant variable.
