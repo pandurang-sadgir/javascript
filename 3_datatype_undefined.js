cd3077629abcd8ec080a2527b651de76d1ad27b1
pandurang sadgir <psadgir111@gmail.com>
2023-03-08 11:00:33 +0530

    undefined datatype

---
+++ 3_datatype_undefined.js
@@ -0,0 +1,11 @@
+
+// variable is declared but not initialized it has defalult value 
+
+var number;
+console.log('value which is store by defalult: ',number);
+console.log('The type of just decalred variable is : ',typeof number);
+
+// output
+// $ node 3_datatype_undefined.js
+// value which is store by defalult:  undefined
+// The type of just decalred variable is :  undefined
