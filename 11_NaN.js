a37b9f1395699d6830571ba8b5c96185e8c3d31b
pandurang sadgir <psadgir111@gmail.com>
2023-03-08 16:48:25 +0530

    NaN number

---
+++ 11_NaN.js
@@ -0,0 +1,24 @@
+
+// NaN is speacial type of numeric value which return when we perform invalid operaton 
+
+var a = 'abcd';
+console.log(a/122);
+
+// output:
+    // NaN
+
+// any opearation with an NaN return NaN
+
+console.log(NaN+10);
+//  output:
+// $ node 11_NaN.js
+// NaN
+
+
+// NaN itsef in not equals to each Other 
+console.log(NaN == NaN);
+
+// output:
+// $ node 11_NaN.js
+// NaN
+// false
--- 5_datatype_number.js
+++ 5_datatype_number.js
@@ -19,3 +19,26 @@ console.log('The type of floating point number is: ',typeof floatNumber);
 // output:
 // The floating point number is 1000.2012161651
 // The type of floating point number is:  number
+
+
+// javascript convert float number to integer if it appears as whole number 
+
+var fNumber = 200.000;
+console.log('The float number is converted to integer',fNumber);
+// output:
+// The float number is converted to integer 200
+
+
+// To get range of number data type.
+
+console.log('The max value of number data type is ',Number.MAX_VALUE);
+console.log('The min value of number data type is ',Number.MIN_VALUE);
+
+// output:
+// The max value of number data type is  1.7976931348623157e+308
+// The min value of number data type is  5e-324
+
+
+// we can also use infinity to represent infinite number 
+console.log('The positive infinite number is' ,Number);
+
