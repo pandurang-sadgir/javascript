96eb4af613b8b0f31bc8cd5084500d68a4678476
pandurang sadgir <psadgir111@gmail.com>
2023-03-08 11:26:07 +0530

    number datatype in js

---
+++ 5_datatype_number.js
@@ -0,0 +1,21 @@
+// javascript uses the number datatype to represent both integer and floatnumber
+
+// js uses number to represent integer 
+
+var intNumber = 10020;
+console.log('The integer number is: ',intNumber );
+console.log('The type of intNumber is : ',typeof intNumber);
+// output:
+// $ node 5_datatype_number.js
+// The integer number is:  10020
+
+
+// js uses the number data type to to represent floating point 
+
+var floatNumber = 1000.2012161651;
+console.log('The floating point number is',floatNumber);
+console.log('The type of floating point number is: ',typeof floatNumber);
+
+// output:
+// The floating point number is 1000.2012161651
+// The type of floating point number is:  number
