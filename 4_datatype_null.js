26e4c5d43d15e32926043bb47181e4748034be9b
pandurang sadgir <psadgir111@gmail.com>
2023-03-08 11:11:01 +0530

    null datatype

---
+++ 4_datatype_null.js
@@ -0,0 +1,18 @@
+// null data type and its typeof
+
+var obj = null;
+console.log('value which is stored in obj is :',obj);
+console.log('The type of obj is :', typeof(obj));
+
+// output:
+// $ node 4_datatype_null.js
+// value which is stored in obj is : null
+// The type of obj is : object
+
+
+// javscript null is equals to undefined
+
+console.log('is null equals to undefined',null == undefined);
+
+// output:
+// is null equals to undefined true
