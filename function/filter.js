afb9fae113dafed58f11477a7b99f731cfea80d4
pandurang sadgir <psadgir111@gmail.com>
2023-05-31 10:34:30 +0530

    filter mathod

---
+++ function/filter.js
@@ -0,0 +1,32 @@
+let userData = [
+    {'user':'ram','age':65},
+    {'user':'shyam','age':75},
+    {'user':'jay','age':23},
+    {'user':'shiv','age':27},
+    {'user':'ketan','age':15},
+    {'user':'chetan','age':55},
+    {'user':'radhe','age':35},
+]
+
+let youngYouth = userData.filter((val,index,arr)=>{
+    if(val.age>20 && val.age<30){
+        return true;//Tis return object value.
+    }else{
+        return false;
+    }
+})
+
+
+console.log(userData);
+console.log(youngYouth);
+// output:
+// [
+//     { user: 'ram', age: 65 },
+//     { user: 'shyam', age: 75 },
+//     { user: 'jay', age: 23 },
+//     { user: 'shiv', age: 27 },
+//     { user: 'ketan', age: 15 },
+//     { user: 'chetan', age: 55 },
+//     { user: 'radhe', age: 35 }
+//   ]
+// [ { user: 'jay', age: 23 }, { user: 'shiv', age: 27 } ]
